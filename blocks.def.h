26a62ecc0ce8ab848087b2c65094ad59c670453c
aeth <alessandrobatta@disroot.org>
2021-10-20 21:37:54 +0200

    time only

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/		    /*Update Interval*/	/*Update Signal*/
-	{"🔋",  "~/.bin/batstat",       60,                 0},
-	{"📦 ", "~/.bin/pacupdate",		360,		        9},
-	{"🕑 ", "~/.bin/clock",			5,		            0},
+	{" 🕑 ", "clock",		      	5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
