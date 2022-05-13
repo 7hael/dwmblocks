8a33ee8f1510a1c7bc7f2c0c4920d0e4f11d4793
aeth_ <ae.thel@disroot.org>
2022-05-13 14:37:18 +0200

    memory status

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/		    /*Update Interval*/	/*Update Signal*/
-	{" 🕑 ", "clock",		      	5,		            0},
+	{" 󰉉 ",  "mem",		      		5,		            0},
+	{"🕑 ", "clock",		      	5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
